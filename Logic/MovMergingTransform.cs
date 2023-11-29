c0acaa5a01a43846ce76d2091178204a12a726a3
Con Nik <conn.nik1@gmail.com>
2023-11-29 18:45:05 +0200

    Update MovMergingTransform

--- Logic/MovMergingTransform.cs
+++ Logic/MovMergingTransform.cs
@@ -10,7 +10,47 @@ namespace Scheduller.Logic
     {
         public string[] Transform(string[] inputCode)
         {
-            throw new NotImplementedException();
+            bool movFound = false;
+            List<string> movOperands = new List<string>();
+            List<string> addOperands = new List<string>();
+
+            for (int i = 0; i < inputCode.Length; i++)
+            {
+                if (inputCode[i].Trim().StartsWith("ADD", StringComparison.OrdinalIgnoreCase))
+                {
+                    if(movFound == true)
+                    {
+                        addOperands = ExtractOperands(inputCode[i]);
+                        if (movOperands[0] == addOperands[1] || movOperands[0] == addOperands[2])
+                        {
+                            Console.WriteLine("true" + " line " + i);
+                        }     
+                        else
+                        {
+                            Console.WriteLine("false");
+                        }
+                    }
+                }
+
+                if (inputCode[i].Trim().StartsWith("MOV", StringComparison.OrdinalIgnoreCase))
+                {
+                    movFound = true;
+                    movOperands = ExtractOperands(inputCode[i]);
+                }
+                else
+                {
+                    movFound = false;
+                }
+            }
+
+            return inputCode;
+        }
+
+        static List<string> ExtractOperands(string instruction)
+        {
+            string[] parts = instruction.Split();
+            List<string> operands = parts.Skip(2).Select(operand => operand.TrimEnd(',')).ToList();
+            return operands;
         }
     }
 }
